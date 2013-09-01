590272c048ad89d4d68cbc16024982ba0a389ca7
ederyair <macbook@MacBook-de-Jair.local>
2013-09-01 14:39:06 -0500

    Gana el Volado
    pequeño jueguito :)

---
+++ Volado/GanaelVolado.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+#include <time.h>
+int  opcion,a,x,b,c,intentos,numero_aleatorio=0;
+char volado;
+main (){
+	clrscr();
+	printf("\n Gana el volado, tienes 5 oportunidades");
+   printf("\n 1) AGUILA");
+   printf("\n 2) SOL");
+
+   printf("\n\n Opcion [ ] \b\b\b");
+   scanf("%d", &opcion);
+   do{
+   	switch(opcion){              
+      	case 1: printf("\n Aguila");
+         scanf("%d", &b);
+         break;
+         case 2: printf("\n Sol");
+         scanf("%d", &c);
+      }
+      intentos++;
+   }while(intentos!=5 && numero_aleatorio != opcion);
+   if (opcion == c)
+   	printf("\n ganaste");
+
+
+   randomize();
+	srand (time(NULL));
+  	numero_aleatorio=0+rand()%2;
+	for (x=0;x < 1; x++){
+	 	numero_aleatorio;
+   	if (numero_aleatorio>=1){
+			printf("\n SOL", &c);
+		}
+		else{
+	  	printf("\n AGUILA", &b);
+   	}
+   }
+
+
+
+
+
+
+getch();
+}
+
+
+
+
+
+
+
+
