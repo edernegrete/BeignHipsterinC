6a07705cd821ae5b0b2cf922dd2df674c6ab1758
ederyair <macbook@MacBook-de-Jair.local>
2013-10-04 23:04:50 -0500

    Ley de Ohm
    Calcula resistencia, voltje e intensidad con respecto a la ley de Ohm.

---
+++ ley ohm.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <conio.h>
+float r,v,i;
+int opcion;
+int main(){
+	
+printf("OPCIONES:");	
+printf("1) Resistencia");
+printf("2) Voltaje");
+printf("3) Intensidad");
+
+printf ("¿Qué quieres calcular? pon el número de la opción [ ]");
+scanf("%d", &opcion);
+switch (opcion)
+{
+	case 1:
+		printf("Voltaje [ ]");
+		scanf("%f",&v);
+		printf("Intensidad [  ] ");
+		scanf("%f", &i);
+		r=v/i;
+		printf("El valor de la resistencia es %f",r);
+	break;
+	case 2:
+		printf("Resistencia [ ]");
+		scanf("%f",&r);
+		printf("Intensidad [ ]");
+		scanf("%f",&i);
+		v=r*i;
+		printf("El voltaje es igual a %f",v);
+	break;
+	case 3:
+		printf("Voltaje [ ]");
+		scanf("%f",&v);
+		printf("Resistencia [ ]");
+		scanf("%f", &r);
+		i=v/r;
+		printf("El valor de la intensidad es %f",i);
+			
+	break;
+	dafault:
+	printf("No seas noob, selecciona una existente");
+}
+
+
+}
